dcafdfe4501065bab6213c6ba17e64425d9431d3
amalsus1995-cpu <am.alsus1995@gmail.com>
2025-12-12 16:24:22 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = "https://hpcipbpgsmvwmkseqfbk.supabase.co";
+const supabaseKey = "sb_publishable_cnwHtamgdBeaN5XrIa0RmA_UAaxCGnS";
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
