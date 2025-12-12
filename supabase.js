4eba4ad9ee4ccabad28150ec126df8e51297680d
amalsus1995-cpu <am.alsus1995@gmail.com>
2025-12-12 16:50:20 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,7 @@
 const supabaseUrl = "https://hpcipbpgsmvwmkseqfbk.supabase.co";
 const supabaseKey = "sb_publishable_cnwHtamgdBeaN5XrIa0RmA_UAaxCGnS";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
